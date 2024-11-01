06bc1260ce9ca424b7d5a82b93ec74d04ebe680d
ouagu <ouaguenia@gmail.com>
2024-11-01 13:50:15 +0100

    Add 2 POJO classes to the main

---
+++ ProjectSkiAziz/src/model/PersonnePOJO.java
@@ -0,0 +1,76 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public abstract class PersonnePOJO implements Serializable {
+    private int id;
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+    
+	public PersonnePOJO(int id, String nom, String prenom, Date dateNaissance) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	@Override
+	public String toString() {
+		return "Personne [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", dateNaissance=" + dateNaissance + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dateNaissance, id, nom, prenom);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonnePOJO other = (PersonnePOJO) obj;
+		return Objects.equals(dateNaissance, other.dateNaissance) && id == other.id && Objects.equals(nom, other.nom)
+				&& Objects.equals(prenom, other.prenom);
+	}
+    
+}
---
+++ ProjectSkiAziz/src/model/SkierPOJO.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.Objects;
+
+public class SkierPOJO {
+    private String niveau;
+    private boolean assurance;
+    
+	public SkierPOJO(String niveau, boolean assurance) {
+		super();
+		this.niveau = niveau;
+		this.assurance = assurance;
+	}
+
+	public String getNiveau() {
+		return niveau;
+	}
+
+	public void setNiveau(String niveau) {
+		this.niveau = niveau;
+	}
+
+	public boolean isAssurance() {
+		return assurance;
+	}
+
+	public void setAssurance(boolean assurance) {
+		this.assurance = assurance;
+	}
+
+	@Override
+	public String toString() {
+		return "Skier [niveau=" + niveau + ", assurance=" + assurance + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(assurance, niveau);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SkierPOJO other = (SkierPOJO) obj;
+		return assurance == other.assurance && Objects.equals(niveau, other.niveau);
+	}
+    
+}
