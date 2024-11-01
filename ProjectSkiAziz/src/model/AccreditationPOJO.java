c3c896ff74aabf918d246abf16e608a3adf32150
ouagu <ouaguenia@gmail.com>
2024-11-01 14:12:35 +0100

    ADD TO MORE POJO ACCREDITATION AND INSTRUCOTOR

---
+++ ProjectSkiAziz/src/model/AccreditationPOJO.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class AccreditationPOJO implements Serializable {
+    private String name;
+    
+	public AccreditationPOJO(String name) {
+		super();
+		this.name = name;
+	}
+    
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "AccreditationPOJO [name=" + name + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccreditationPOJO other = (AccreditationPOJO) obj;
+		return Objects.equals(name, other.name);
+	}
+    
+}
+
---
+++ ProjectSkiAziz/src/model/InstructorPOJO.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+public class InstructorPOJO extends PersonnePOJO {
+    private int experience;
+    private List<String> certifications = new ArrayList<>();
+    
+	public InstructorPOJO(int id, String nom, String prenom, Date dateNaissance, int experience,
+			List<String> certifications) {
+		super(id, nom, prenom, dateNaissance);
+		this.experience = experience;
+		this.certifications = certifications;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+	public List<String> getCertifications() {
+		return certifications;
+	}
+
+	public void setCertifications(List<String> certifications) {
+		this.certifications = certifications;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorPOJO [experience=" + experience + ", certifications=" + certifications + ", getId()="
+				+ getId() + ", getNom()=" + getNom() + ", getPrenom()=" + getPrenom() + ", getDateNaissance()="
+				+ getDateNaissance() + ", toString()=" + super.toString() + ", hashCode()=" + hashCode()
+				+ ", getClass()=" + getClass() + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(certifications, experience);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InstructorPOJO other = (InstructorPOJO) obj;
+		return Objects.equals(certifications, other.certifications) && experience == other.experience;
+	}
+    
+}
+
